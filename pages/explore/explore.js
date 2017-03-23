16f15f85553d760365fe11a5b18304cfe074f4f3
KainyGuo <gt@kainy.cn>
2017-03-23 15:01:21 +0800

    eslint。

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -1,5 +1,5 @@
 const api = require('../../utils/api.js');
-const WxSearch = require('../../components/wxSearch/wxSearch.js')
+const WxSearch = require('../../components/wxSearch/wxSearch.js');
 const util = require('../../utils/util.js');
 
 const App = getApp();
@@ -9,7 +9,7 @@ Page({
     elements: [],
     windowWidth: App.systemInfo.windowWidth,
     searchResult: {
-      trips: []
+      trips: [],
     },
     start: 0,
     trips_more: false,
@@ -22,9 +22,9 @@ Page({
       icon: 'loading',
       duration: 10000,
     });
-    WxSearch.init(this,62,['蜜月','毕业','跨年','哈尔滨','浪漫']);
-    WxSearch.initMindKeys(['香港','澳门','泰国','德国','北京','英国','法国','哈尔滨','三亚']);
-    if(options.keyword){
+    WxSearch.init(this, 62, ['蜜月', '毕业', '跨年', '哈尔滨', '浪漫']);
+    WxSearch.initMindKeys(['香港', '澳门', '泰国', '德国', '北京', '英国', '法国', '哈尔滨', '三亚']);
+    if (options.keyword) {
       this.wxSearchFn(null, decodeURIComponent(options.keyword));
     }
     api.getExplorePlaceList({
@@ -38,18 +38,18 @@ Page({
     });
   },
   onShareAppMessage() {
-    var kw = this.data.wxSearchData.value || ''
+    const kw = this.data.wxSearchData.value || '';
     const opt = {
       title: `发现${kw} ✈️ 跨时空旅行日志`,
       desc: '探索最佳旅行地、热门地点、欧美国家、港澳台、亚洲国家…',
-      path: `/pages/explore/explore?keyword=${encodeURIComponent(kw)}`
-    }
-    console.log(opt)
-    return opt
+      path: `/pages/explore/explore?keyword=${encodeURIComponent(kw)}`,
+    };
+    console.log(opt);
+    return opt;
   },
-  onReachBottom: function () {
-    if(this.data.trips_more && !this.fetching){
-      this.wxSearchFn(null, null , true)
+  onReachBottom() {
+    if (this.data.trips_more && !this.fetching) {
+      this.wxSearchFn(null, null, true);
     }
   },
   viewPOI(e) {
@@ -64,98 +64,97 @@ Page({
       url: `../trip/trip?id=${ds.id}&name=${ds.name}`,
     });
   },
-  wxSearchFn: function(e, kw, append){
-    var self = this
-    var temData = self.data.wxSearchData
-    kw = kw || temData.value;
+  wxSearchFn(e, kw, append) {
+    const self = this;
+    const temData = self.data.wxSearchData;
+    kw = kw || temData.value; // eslint-disable-line
     if (!append) {
       // 搜索结果复位
       this.setData({
         start: 0,
         trips_more: false,
         searchResult: {
-          trips: []
+          trips: [],
         },
         searching: true,
-      })
+      });
     }
     if (!kw) {
       this.setData({
         searchResult: {
-          trips: []
+          trips: [],
         },
         searching: false,
-      })
+      });
       WxSearch.wxSearchHiddenPancel(self);
-      return
-    } else {
-      temData.value = kw
-      this.setData({
-        wxSearchData: temData
-      })
+      return;
     }
-    self.fetching = true
+    temData.value = kw;
+    this.setData({
+      wxSearchData: temData,
+    });
+
+    self.fetching = true;
     api.search({
       data: {
         key: kw,
         start: self.data.start,
         count: pageSize,
-        data_type: 'trip'
+        data_type: 'trip',
       },
       success: (res) => {
-        if(res.data.status == 0){
-          const len = res.data.data.trips.length
-          if(len){
+        if (res.data.status === 0) {
+          const len = res.data.data.trips.length;
+          if (len) {
             self.setData({
               searchResult: {
-                trips: self.data.searchResult.trips.concat(res.data.data.trips)
+                trips: self.data.searchResult.trips.concat(res.data.data.trips),
               },
               start: self.data.start + len,
-              trips_more: res.data.data.trips_more
-            })
+              trips_more: res.data.data.trips_more,
+            });
           } else {
-            util.alert('未找到相关内容，换个关键词试试 ？')
+            util.alert('未找到相关内容，换个关键词试试 ？');
           }
         } else {
-          util.alert(res.data.message)
+          util.alert(res.data.message);
         }
-        self.fetching = false
+        self.fetching = false;
         self.setData({
           searching: false,
-        })
+        });
         // console.log(trips)
-      }
-    })
+      },
+    });
     WxSearch.wxSearchAddHisKey(self);
-    
   },
-  wxSearchInput: function(e){
-    var self = this
-    WxSearch.wxSearchInput(e,self);
+  wxSearchInput(e) {
+    const self = this;
+    WxSearch.wxSearchInput(e, self);
   },
-  wxSerchFocus: function(e){
-    var self = this
-    WxSearch.wxSearchFocus(e,self);
+  wxSerchFocus(e) {
+    const self = this;
+    WxSearch.wxSearchFocus(e, self);
   },
-  wxSearchBlur: function(e){
-    var self = this
-    WxSearch.wxSearchBlur(e,self);
+  wxSearchBlur(e) {
+    const self = this;
+    WxSearch.wxSearchBlur(e, self);
   },
-  wxSearchKeyTap:function(e){
-    var self = this
-    WxSearch.wxSearchKeyTap(e,self);
+  wxSearchKeyTap(e) {
+    const self = this;
+    WxSearch.wxSearchKeyTap(e, self);
     self.wxSearchFn();
   },
-  wxSearchDeleteKey: function(e){
-    var self = this
-    WxSearch.wxSearchDeleteKey(e,self);
+  wxSearchDeleteKey(e) {
+    const self = this;
+    WxSearch.wxSearchDeleteKey(e, self);
   },
-  wxSearchDeleteAll: function(e){
-    var self = this;
+  wxSearchDeleteAll() {
+    const self = this;
     WxSearch.wxSearchDeleteAll(self);
   },
-  wxSearchTap: function(e){
-    var self = this
+  wxSearchTap() {
+    const self = this;
     WxSearch.wxSearchHiddenPancel(self);
-  }
+  },
 });
