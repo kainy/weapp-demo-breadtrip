c3889cfa61d99353122ac1e3907fba9d611e0d50
KainyGuo <gt@kainy.cn>
2021-04-22 20:02:07 +0800

    探索tab，搜索。

--- miniprogram/pages/explore/explore.js
+++ miniprogram/pages/explore/explore.js
@@ -4,36 +4,45 @@ const util = require('../../utils/util.js');
 
 const App = getApp();
 const pageSize = 17;
+const arrKeyword = ['蜜月', '毕业', '跨年', '冲绳', '浪漫', '小清新']
 Page({
   data: {
     elements: [],
     windowWidth: App.systemInfo.windowWidth,
-    searchResult: {
+    // searchResult: {
+    //   trips: [],
+    // },
+    wxOpenSearch: {
       trips: [],
+      nextPageInfo: null,
     },
     start: 0,
     trips_more: false,
     searching: false,
     searchFocus: false,
   },
+  getKW() {
+    return arrKeyword[Math.round(arrKeyword.length * Math.random())]
+  },
   onLoad(options) {
     const self = this;
     util.showLoading();
-    WxSearch.init(this, 62, ['蜜月', '毕业', '跨年', '冲绳', '浪漫', '小清新']);
+    WxSearch.init(this, 62, arrKeyword);
     WxSearch.initMindKeys(['香港', '澳门', '泰国', '德国', '北京', '英国', '法国', '哈尔滨', '三亚']);
-    if (options.keyword) {
-      this.wxSearchFn(null, decodeURIComponent(options.keyword));
-    }
-    api.getExplorePlaceList({
-      success: (res) => {
-        const dest = res.data;
-        self.setData({
-          elements: dest.elements,
-          searchFocus: true,
-        });
-        util.hideLoading();
-      },
-    });
+    const keyw = decodeURIComponent(options.keyword || this.getKW())
+    // if (options.keyword) {
+      this.wxSearchFn(null, keyw);
+    // }
+    // api.getExplorePlaceList({
+    //   success: (res) => {
+    //     const dest = res.data;
+    //     self.setData({
+    //       elements: dest.elements,
+    //       // searchFocus: true,
+    //     });
+    //     util.hideLoading();
+    //   },
+    // });
   },
   onShareAppMessage() {
     const kw = this.data.wxSearchData.value || '';
@@ -62,24 +71,42 @@ Page({
       url: `../trip/trip?id=${ds.id}&name=${ds.name}`,
     });
   },
+  viewPath(e) {
+    const ds = e.currentTarget.dataset;
+    const url = '/' + ds.path.replace('.html', '')
+    console.log('url:', url)
+    wx.navigateTo({
+      url
+    });
+  },
   wxSearchFn(e, kw, append) {
     const self = this;
     const temData = self.data.wxSearchData;
     kw = kw || temData.value; // eslint-disable-line
+    kw = kw || this.getKW(); // eslint-disable-line
+    wx.setNavigationBarTitle({
+      title: `关于 ${kw} 的旅行信息，探索最佳旅行地、热门地点、欧美国家、港澳台、亚洲国家…`
+    })
     if (!append) {
       // 搜索结果复位
       this.setData({
         start: 0,
         trips_more: false,
-        searchResult: {
-          trips: [],
+        // searchResult: {
+        //   trips: [],
+        // },
+        wxOpenSearch: {
+          trips: []
         },
         searching: true,
       });
     }
     if (!kw) {
       this.setData({
-        searchResult: {
+        // searchResult: {
+        //   trips: [],
+        // },
+        wxOpenSearch: {
           trips: [],
         },
         searching: false,
@@ -93,23 +120,60 @@ Page({
     });
 
     self.fetching = true;
-    api.search({
+    // api.search({
+    //   data: {
+    //     key: kw,
+    //     start: self.data.start,
+    //     count: pageSize,
+    //     data_type: 'trip',
+    //   },
+    //   success: (res) => {
+    //     const len = res.data.data.trips.length;
+    //     if (len) {
+    //       self.setData({
+    //         searchResult: {
+    //           trips: self.data.searchResult.trips.concat(res.data.data.trips),
+    //         },
+    //         start: self.data.start + len,
+    //         trips_more: res.data.data.trips_more,
+    //       });
+    //     } else {
+    //       util.alert('未找到相关内容，换个关键词试试吧');
+    //     }
+    //     self.fetching = false;
+    //     self.setData({
+    //       searching: false,
+    //     });
+    //     // console.log(trips)
+    //   },
+    // });
+    wx.cloud.callFunction({
+      name: 'openapi',
       data: {
-        key: kw,
-        start: self.data.start,
-        count: pageSize,
-        data_type: 'trip',
+        action: 'search',
+        keyword: kw,
+        nextPageInfo: self.data.wxOpenSearch.nextPageInfo
       },
-      success: (res) => {
-        const len = res.data.data.trips.length;
+      success: res => {
+        util.hideLoading();
+        // console.warn(res, 88)
+        const list = res.result.items.map((item, index) => {
+          item.title = item.title.replace(/(\<em class\=\"highlight\"\>)|(\<\/em\>)/ig, '');
+          item.day_count = Math.round(Math.random() * 12) + 3
+          item.waypoints = Math.round(Math.random() * 56) + 7
+          item.recommendations = Math.round(Math.random() * 789) + 10
+          return item
+        });
+        const len = list.length;
         if (len) {
           self.setData({
-            searchResult: {
-              trips: self.data.searchResult.trips.concat(res.data.data.trips),
+            wxOpenSearch: {
+              trips: self.data.wxOpenSearch.trips.concat(list),
+              nextPageInfo: res.result.nextPageInfo,
+              hitCount: res.result.hitCount
             },
-            start: self.data.start + len,
-            trips_more: res.data.data.trips_more,
-          });
+            trips_more: !!res.result.hasNextPage,
+          })
         } else {
           util.alert('未找到相关内容，换个关键词试试吧');
         }
@@ -118,20 +182,10 @@ Page({
           searching: false,
         });
         // console.log(trips)
-      },
+        
+      }
     });
     WxSearch.wxSearchAddHisKey(self);
-
-    wx.cloud.callFunction({
-      name: 'openapi',
-      data: {
-        action: 'search',
-        keyword: kw
-      },
-      success: res => {
-        console.warn(res, 88)
-      }
-    })
   },
   clearInput() {
     const temData = this.data.wxSearchData;
